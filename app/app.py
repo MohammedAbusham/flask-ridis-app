ee51e6c14e4516859de10db846825f9e215d88be
Mohammed Hussien <moh.abusham@gmail.com>
2025-11-05 23:12:58 +0000

    Final update, for redisconf, Dockerfile and docker-compose

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ import redis
 app = Flask(__name__)
 
 @app.route('/')
-def welcome_message()
+def welcome_message():
     # Displays a welcome message.
     return 'Hello, you are connected to My Fask App'
 
 
 @app.route('/count')
-def vist_count()
+def vist_count():
     # Connect and displays a visit count stored in RedisDB.
     r = redis.Redis(host='redisdb', port=6379, db=0, decode_responses=True)
 
@@ -20,7 +20,7 @@ def vist_count()
     # Retrieve the counted vists
     count = r.get('page_visits')
 
-    return f"<h1>WOW!</h1><p>Moe's web App has been visited <b>{count}</b> times.
+    return f"<h1>WOW!</h1><p>Moe's web App has been visited <b>{count}</b> times."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
