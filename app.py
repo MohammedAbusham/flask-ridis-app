3f9959445bb876125c75bab177ed3c2166213444
Mohammed Hussien <moh.abusham@gmail.com>
2025-11-08 10:06:29 +0000

    Updated the app to work on ENV variables

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
 import redis
 
 app = Flask(__name__)
+# Connect RedisDB.
+redis_host= os.getenv('REDIS_HOST','redis')
+redis_port= int(os.getenv('REDIS_PORT', 6379))
+r = redis.Redis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def welcome_message():
@@ -11,13 +16,13 @@ def welcome_message():
 
 @app.route('/count')
 def vist_count():
-    # Connect and displays a visit count stored in RedisDB.
-    r = redis.Redis(host='redis', port=6379, db=0)
-
+    # Dsplays a visit count stored in RedisDB.
+    r = redis.Redis(host=redis_host, port=redis_port, db=0)
     # Increment the page visit counter
     visits = r.incr('page_visits')
 
     return f"<h1>WOW!</h1><p>Moe's web App has been visited <b>{visits}</b> times."
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
